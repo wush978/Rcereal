3831a5bfb1f2542078d5164a1a3b5214006587a5
Wush Wu <wush978@gmail.com>
2015-10-31 14:38:23 +0800

    use environment variable to control testing target

--- tests/testthat/test-install_github.R
+++ tests/testthat/test-install_github.R
@@ -1,7 +1,9 @@
 context("Test the instruction of installation via github_install")
 
 test_that("Install the package via github_install", {
-  devtools::install_github("wush978/Rcereal@master")
-  update_version("1.1.2")
-  expect_true("include" %in% dir(system.file("", package = "Rcereal")))
+  if (Sys.getenv("RCEREAL_TEST_GITHUB" == "TRUE")) {
+    devtools::install_github("wush978/Rcereal@master")
+    update_version("1.1.2")
+    expect_true("include" %in% dir(system.file("", package = "Rcereal")))
+  }
 })
