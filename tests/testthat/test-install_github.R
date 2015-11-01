81152f7fd217e7467dc7cd1a6a725f3ba267906b
Wush Wu <wush978@gmail.com>
2015-11-01 15:02:58 +0800

    debug: syntax error of testing environment variable

--- tests/testthat/test-install_github.R
+++ tests/testthat/test-install_github.R
@@ -1,7 +1,7 @@
 context("Test the instruction of installation via github_install")
 
 test_that("Install the package via github_install", {
-  if (Sys.getenv("RCEREAL_TEST_GITHUB" == "TRUE")) {
+  if (Sys.getenv("RCEREAL_TEST_GITHUB") == "TRUE") {
     devtools::install_github("wush978/Rcereal@master")
     update_version("1.1.2")
     expect_true("include" %in% dir(system.file("", package = "Rcereal")))
