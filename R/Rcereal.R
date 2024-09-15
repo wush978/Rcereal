f738098ed4587f95fc4e227e9f0e697b6573ba45
stephematician <stephematician@gmail.com>
2024-09-15 16:00:21 +1000

    Add dots to update_version
    Add dots to pass arguments to system.file in update_version
    Minor tidy of inline documentation

--- R/Rcereal.R
+++ R/Rcereal.R
@@ -1,63 +1,94 @@
-#'@title List the version of cereal on github.
-#'@return package_version. The vector of versions of cereal.
-#'@details Use the github API to query the versions of cereal.
-#'  The github page of cereal is \url{https://github.com/USCiLab/cereal}
-#'  This function requires the package \code{httr}.
-#'@export
+#' List version(s) of cereal on GitHub.
+#'
+#' Use the GitHub API to query the versions of cereal.
+#'
+#' The GitHub page of cereal is <https://github.com/USCiLab/cereal>, the
+#' tags are accessed via the GitHub API, from which a `package_version` object
+#' is coerced.
+#'
+#' @return `package_version`, a vector of available versions.
+#'
+#' @export
+#' @md
 list_version <- function() {
-  response <- httr::GET("https://api.github.com/repos/USCiLab/cereal/tags")
-  stopifnot(httr::status_code(response) == 200)
-  v <- sapply(httr::content(response), function(obj) {
-    substring(obj$name, 2, nchar(obj$name))
-  })
-  package_version(v)
+
+    response <- httr::GET("https://api.github.com/repos/USCiLab/cereal/tags")
+    stopifnot(httr::status_code(response) %in% 200L)
+
+    tag <- sapply(httr::content(response), function(obj) {
+        substring(obj$name, 2L, nchar(obj$name))
+    })
+
+    package_version(tag)
+
 }
 
-#'@title Return the latest version of cereal on the github.
-#'@return package_version. The latest version of cereal.
-#'@details This function requires the package \code{httr}.
-#'@export
-last_version <- function() {
-  max(list_version())
-}
+#' Return the latest version of cereal on the GitHub.
+#'
+#' Uses the GitHub API to find the latest version of cereal.
+#'
+#' Gets all the versions from GitHub via [list_version()] and selects the
+#' largest version number.
+#'
+#' @return `package_version`
+#' @export
+#' @md
+last_version <- function() max(list_version())
 
-.package_file <- function(...) {
-  system.file(..., package = .packageName)
-}
 
-.path <- "https://github.com/USCiLab/cereal.git"
+# Helper for finding location of header files
+.package_file <- function(...) system.file(..., package = .packageName)
 
-#'@title Update the cereal to the Given Version
-#'@param version package_version. The version of cereal
-#'  you want to switch in Rcereal.
-#'@details This function will change the version of cereal
-#'  in Rcereal. The package \code{git2r} is required.
-#'@export
-update_version <- function(version = last_version()) {
-  stopifnot(length(version) == 1)
-  if (class(version)[1] == "character") {
-    if (substring(version, 1, 1) == "v") {
-      version <- substring(version, 2, nchar(version))
+
+#' Update installed cereal headers
+#'
+#' Clone a different version of the cereal headers into R library.
+#'
+#' This over-writes the installed cereal headers inside an R library. The
+#' default location for the files is found via [system.file()]. The library
+#' location can be specified by passing an argument `lib.loc`. See
+#' [system.file()] for further details.
+#'
+#' @param version `character` or [package_version]; the version to install, e.g.
+#' '1.3.2' or `v1.3.2`.
+#' @param ... additional arguments passed to [system.file()], e.g. `lib.loc` for
+#' the location of the library that Rcereal is installed in.
+#'
+#' @export
+#' @md
+update_version <- function(version=last_version(), ...) {
+
+    stopifnot(length(version) == 1L,
+              is.character(version) || is.package_version(version))
+
+    if (is.character(version)) {
+        if (substring(version, 1L, 1L) == "v")
+            version <- substring(version, 2L, nchar(version))
     }
     version <- package_version(version)
-  }
-  stopifnot(class(version)[1] == "package_version")
-  .tmppath <- tempfile()
-  dir.create(.tmppath)
-  git2r::clone(.path, .tmppath)
-  .repo <- git2r::repository(.tmppath)
-  .commit <- git2r::tags(.repo)[[sprintf("v%s", version)]]
-  git2r::checkout(.commit)
-  .dst <- file.path(.package_file(""), "include")
-  if (!file.exists(.dst)) dir.create(.dst)
-  stopifnot(file.rename(.dst, .include <- file.path(.package_file(""), ".include")))
-  tryCatch({
-    .src <- file.path(.tmppath, "include")
-    file.copy(.src, .package_file(""), overwrite = TRUE, recursive = TRUE)
-    stopifnot(dir(.dst) == "cereal")
-    unlink(.include, recursive = TRUE)
-  }, error = function(e) {
-    file.rename(.include, .dst)
-    stop(conditionMessage(e))
-  })
+
+    .tmppath <- tempfile()
+    dir.create(.tmppath)
+    git2r::clone("https://github.com/USCiLab/cereal.git", .tmppath)
+    .repo <- git2r::repository(.tmppath)
+    .commit <- git2r::tags(.repo)[[sprintf("v%s", version)]]
+    git2r::checkout(.commit)
+
+    .dst <- file.path(.package_file("", ...), "include")
+    stopifnot(
+        file.rename(.dst,
+                    .include <- file.path(.package_file("", ...), ".include"))
+    )
+
+    tryCatch({
+        .src <- file.path(.tmppath, "include")
+        file.copy(.src, .package_file("", ...), overwrite=TRUE, recursive=TRUE)
+        stopifnot(dir(.dst) == "cereal")
+        unlink(.include, recursive=TRUE)
+    }, error = function(e) {
+        file.rename(.include, .dst)
+        stop(conditionMessage(e))
+    })
+
 }
+
